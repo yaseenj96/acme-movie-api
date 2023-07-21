abda8f5dba76b6a7b96cd569b2cf804e62f7c8ed
Yaseen Jeber <yaseenj96@gmail.com>
2023-07-20 19:36:56 -0500

    second test

--- test/spec.js
+++ test/spec.js
@@ -3,8 +3,8 @@ const { expect } = require("chai");
 //Test the first test
 describe("test", () => {
   it("should return true", () => {
-    const truthy = true;
+    const truthy = false;
 
-    expect(truthy).to.equal(true);
+    expect(truthy).to.equal(false);
   });
 });
